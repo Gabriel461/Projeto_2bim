0ca6dc178d0d122936398765705019579c13f5a8
Pedro Davi <pdrocavfar@gmail.com>
2026-06-25 16:16:13 -0300

    jhQOUDUWADUWAEHU

--- Projeto_Ariadne/login.js
+++ Projeto_Ariadne/login.js
@@ -1,20 +1,17 @@
 const form = document.getElementById("login");
-const perfil = JSON.parse(LocalStorage.getItem("perfil"));
 
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const emailConta = document.getElementById("email").value;
+    const senhaConta = document.getElementById("senha").value;
+    const perfil = JSON.parse(localStorage.getItem("perfil"));
 
-        form.addEventListener("submit", function(trazer){
-            trazer.preventDefault();
-            const emailConta = document.getElementById("email").value;
-            const senhaConta = document.getElementById("senha").value;
+    if (emailConta === perfil.email && senhaConta === perfil.senha) {
+        let msg = `Seja bem vindo de novo ${perfil.nome}`;
+        alert(msg);
+        window.location.href = "site.html";
+    } else {
+        alert("Email ou senha incorretos! Entre com email ou senha corretos.")
+    }
 
-        const perfil = JSON.parse(localStorage.getItem("perfil"));  
-        
-        if (emailConta === perfil.email && senhaConta === perfil.senha){
-            let msg = `Seja bem vindo de novo ${perfil.nome}`;
-            alert(msg);
-            window.location.href = "site.html";
-        } else{
-            alert("Email ou senha incorretos! Entre com email ou senha corretos.")
-        }
-
-        });
+});
