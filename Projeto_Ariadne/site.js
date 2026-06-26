2306c393865263c75aaa675236bab1abf307f8f5
Andremiguel21 <andremiguelcfaria9@gmail.com>
2026-06-25 22:36:29 -0300

    ed

--- Projeto_Ariadne/site.js
+++ Projeto_Ariadne/site.js
@@ -1,3 +1,6 @@
-const buscar = document.getElementById("buscar");
-const resultado = document.getElementById("resultado");
+const botao = document.getElementById("menu_btn");
+const menu = document.querySelector("aside");
 
+botao.addEventListener("click", function () {
+    menu.classList.toggle("fechado");
+});
